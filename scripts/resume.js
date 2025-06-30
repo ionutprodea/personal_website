49ceacd0b602cf3f7b4afef0b219972a8b1d4312
ionutprodea <147700111+ionutprodea@users.noreply.github.com>
2025-06-30 18:28:25 +0300

    Updated resume page and scripts

--- scripts/resume.js
+++ scripts/resume.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    const assetsResponsible = document.getElementById('assets responsible');
+    if (assetsResponsible) {
+        const applyAnimation = () => {
+            if(window.innerWidth > 767) {
+                assetsResponsible.classList.add('animate__animated');
+            } else {
+                assetsResponsible.classList.remove('animate__animated');
+            }
+        }
+        applyAnimation();
+    }
+    
     const systemEngineer = document.getElementById('system engineer');
     if (systemEngineer) {
         const applyAnimation = () => {
